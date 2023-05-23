8d4f4f6a889f26f6aa9d88531add35b83782e76f
Jose Pena <josehpena2@gmail.com>
2023-05-22 21:38:06 -0300

    Second test, testing type 1

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,11 @@
 const { findYourCaterer } = require('./../src/index.js');
 
 describe('findYourCaterer', () => {
-    it('Should return -1 when the budget sended is 0 and the number of person is 1', () => {
-        expect(findYourCaterer(0,1)).toBe(-1);
-    });
+  it('Should return -1 when the budget sended is 0 and the number of person is 1', () => {
+    expect(findYourCaterer(0, 1)).toBe(-1);
+  });
+
+  it('Should return type 1 when the budget and people sended are 15 and 1 respectively ', () => {
+    expect(findYourCaterer(15, 1)).toBe(1);
+  });
 });
